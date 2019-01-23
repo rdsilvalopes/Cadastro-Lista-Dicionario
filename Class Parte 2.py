ad867cc0d39be74026e2d2bbc16466a9c467061a
rdsilvalopes <37719443+rdsilvalopes@users.noreply.github.com>
2019-01-23 19:18:39 -0200

    Add files via upload
    Menu apagar incluido.
    E condigo incluido.
    
        elif op == 'a'.upper():
            print('\nInforme o ISBN do livro que deseja APAGAR: ')
            a.isbn = int(input('ISBN: '))
    
            if a.isbn in a.bd_livraria:
                del a.bd_livraria[a.isbn]
            else:
                print('>>> ISBN não encontrado! <<<')

--- Class Parte 2.py
+++ Class Parte 2.py
@@ -14,8 +14,9 @@ class Livraria:
         self.isbn = valor
         a.lista = a.lista_temporaria.copy()
         a.bd_livraria[a.isbn] = a.lista
-        a.lista.append(str(a.titulo))
         a.lista.append(str(a.autor))
+        a.lista.append(str(a.titulo))
+
 
         return self.isbn
 
@@ -25,7 +26,7 @@ a = Livraria(idd='', nome='', escritor='')
 while True:
     print('')
     print('\033[7;30mCADASTRAR NOVO LIVRO\033[m\n')
-    print('N - Novo Cadastro\nR - Relatório\nS - Sair')
+    print('N - Novo Cadastro\nR - Relatório\nA - Apagar\nS - Sair')
     print('')
 
     op = str(input('Escolha uma opção: ')).upper()
@@ -51,8 +52,17 @@ while True:
             y = a.bd_livraria.items()
             for k, v in y:
                 print('ISBN: ', k, end=" - ")
-                print('Autor: ', v[1], end=" - ")
-                print('Livro: ', v[0])
+                print('\033[30mLIVRO:\033[m ', v[1], end=" - ")
+                print('\033[30mAUTOR:\033[m ', v[0])
+
+    elif op == 'a'.upper():
+        print('\nInforme o ISBN do livro que deseja APAGAR: ')
+        a.isbn = int(input('ISBN: '))
+
+        if a.isbn in a.bd_livraria:
+            del a.bd_livraria[a.isbn]
+        else:
+            print('>>> ISBN não encontrado! <<<')
 
     elif op == 's'.upper():
         print('Sair')
